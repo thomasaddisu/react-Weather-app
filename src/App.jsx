9ff6a2a63a990e4c6d5eb4215ba96a5783b7cbe2
V empire <thomas.addisu@aastustudent.edu.et>
2026-02-22 10:37:50 +0300

    add functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,39 @@
-import { useState } from 'react'
-
-import './App.css'
-import WeatherApp from './components/weatherApp'
+import { useEffect, useState } from "react";
+import axios from "axios";
+import "./App.css";
+import WeatherApp from "./components/weatherApp";
 
 function App() {
-  const [count, setCount] = useState(0)
+  // weather app state and logic can be added here in the future
+  const [weatherData, setWeatherData] = useState(null);
+  // lets controle city is not correct
+  
+
+  const fetchWeatherData = async (location="Addis Ababa") => {
+
+    const apiKey = "9537e31c70f9105cbabb8c63d1fc3c27";
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}&units=metric`;
+
+    
+    try {
+      const response = await axios.get(apiUrl);
+      const data = response.data;
+      setWeatherData(data);
+      // console.log(weatherData)
+    } catch (error) {
+      console.error("Error fetching weather data:", error);
+      setWeatherData(null);
+    }
+  };
+  useEffect(() => {
+    fetchWeatherData();
+  }, []);
 
   return (
     <>
-    <WeatherApp />
-
+      <WeatherApp weatherData={weatherData} onSearch={fetchWeatherData} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/weatherApp.jsx
+++ src/components/weatherApp.jsx
@@ -1,38 +1,75 @@
 import "./WeatherApp.css";
+import { useState } from "react";
 
-function WeatherApp() {
+function WeatherApp({ weatherData, onSearch }) {
+  const [city, setCity] = useState("addis ababa");
+
+  const handleSearch = () => {
+    if (city.trim() !== "") {
+      onSearch(city);
+    }
+    setCity("");
+  };
+
+  const today = new Date();
+  const options = { weekday: "long", day: "numeric", month: "long" };
+  const formattedDate = today.toLocaleDateString("en-US", options);
+
+  console.log(formattedDate);
+
+  if (!weatherData) {
+    return (
+      <div className="weather">
+        <div className="weather__card ">
+          <div className="weather__search">
+            <input type="text" placeholder="Search city..." />
+            <button>Search</button>
+          </div>
+          <div className="weather__card-loading">
+            ...Loading
+          </div>
+        </div>
+      </div>
+    );
+  }
   return (
     <div className="weather">
       <div className="weather__card">
-
         <div className="weather__search">
-          <input type="text" placeholder="Search city..." />
-          <button>Search</button>
+          <input type="text" placeholder="Search city..." 
+          value={city}
+          onChange={(e) => setCity(e.target.value)}
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              handleSearch();
+            }
+          }}
+          />
+          <button onClick={handleSearch}>Search</button>
         </div>
 
         <div className="weather__info">
-          <h2 className="weather__city">Addis Ababa</h2>
-          <p className="weather__date">Monday, 21 February</p>
+          <h2 className="weather__city">{weatherData.name}</h2>
+          <p className="weather__date">{formattedDate}</p>
 
           <div className="weather__main">
             <div className="weather__icon">☀️</div>
-            <div className="weather__temp">24°C</div>
+            <div className="weather__temp">{weatherData.main.temp}°C</div>
           </div>
 
-          <p className="weather__desc">Sunny</p>
+          <p className="weather__desc">{weatherData.weather[0].description}</p>
         </div>
 
         <div className="weather__details">
           <div className="weather__detail">
-            <span>Humidity</span>
-            <strong>65%</strong>
+            <span>Humidity </span>
+            <strong>{weatherData.main.humidity}%</strong>
           </div>
           <div className="weather__detail">
-            <span>Wind</span>
-            <strong>12 km/h</strong>
+            <span>Wind </span>
+            <strong>{weatherData.wind.speed} km/h</strong>
           </div>
         </div>
-
       </div>
     </div>
   );
